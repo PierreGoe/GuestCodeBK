75683a522cf858293b3ddaaa604b5425c611a572
PierreGoe <57042206+PierreGoe@users.noreply.github.com>
2021-11-01 13:08:18 +0100

    Add function for send mail

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const puppeteer = require('puppeteer');
 const cliProgress = require('cli-progress');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+let APP = process.env.APP;
+let PASS = process.env.PASS;
+let USERS = process.env.USERS.split(' , ');
+
+console.log(USERS.length);
 
 // create new progress bar
 const b1 = new cliProgress.SingleBar({
@@ -11,6 +18,14 @@ const b1 = new cliProgress.SingleBar({
 // initialize the bar - defining payload token "speed" with the default value "N/A"
 console.log('Work in progress, please wait.');
 b1.start(20, 0);
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: APP,
+    pass: PASS,
+  },
+});
 
 (async () => {
   // Go to Final page of Form.
@@ -30,7 +45,29 @@ b1.start(20, 0);
   const element = await page.$('.ValCode');
   const element_property = await element.getProperty('innerHTML');
   const inner_html = await element_property.jsonValue();
-  const result = inner_html.replace('Validierungscode: ', '');
-  console.log(result);
+  const result = await inner_html.replace('Validierungscode: ', '');
+  let mailOption = await [
+    {
+      from: 'botmailpg@gmail.com',
+      to: USERS[0],
+      subject: 'The Magik Code for BK',
+      text: result,
+    },
+    {
+      from: 'botmailpg@gmail.com',
+      to: USERS[1],
+      subject: 'The Magik Code for BK',
+      text: result,
+    },
+  ];
   await browser.close();
+  for (let index = 0; index < USERS.length; index++) {
+    await transporter.sendMail(mailOption[index], function (err, succes) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Email send');
+      }
+    });
+  }
 })();
