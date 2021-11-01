5227929bab8a96eda110f446d405266be02ba88a
PierreGoe <57042206+PierreGoe@users.noreply.github.com>
2021-11-01 10:06:05 +0100

    Add progression bar

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const puppeteer = require('puppeteer');
+const cliProgress = require('cli-progress');
+
+// create new progress bar
+const b1 = new cliProgress.SingleBar({
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+  hideCursor: true,
+});
+
+// initialize the bar - defining payload token "speed" with the default value "N/A"
+console.log('Work in progress, please wait.');
+b1.start(20, 0);
 
 (async () => {
   // Go to Final page of Form.
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.bk-feedback-de.com/Index.aspx?c=051011');
+
   for (let index = 0; index < 21; index++) {
     await page
       .waitForSelector('#NextButton')
-      .then(() => page.click('#NextButton'));
+      .then(() => page.click('#NextButton'))
+      .then(() => b1.update(index));
   }
+  b1.stop();
 
   // Take result in Final page.
   const element = await page.$('.ValCode');
